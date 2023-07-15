8a9422f7b44bad7b719c35c8dcfb432ee286b450
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-07-15 16:32:30 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const listOfIngredients = [];
+
+ingredients.forEach(function (ingredient) {
+  const liEl = document.createElement('li');
+
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+
+  listOfIngredients.push(liEl);
+});
+
+// console.log(listOfIngredients);
+ingredientsEl.append(...listOfIngredients);
