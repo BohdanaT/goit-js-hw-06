53fdd07431681ce91be87fce8a860dca66f1aab7
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-07-16 23:15:54 +0300

    task-7

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const fontSizeControlBarEl = document.querySelector('#font-size-control');
+const textEl = document.querySelector('#text');
+
+
+fontSizeControlBarEl.addEventListener('input', onSizeControlBarChange);
+
+function onSizeControlBarChange(event) {
+   textEl.style.fontSize = event.target.value + 'px';
+}
