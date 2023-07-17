0d18923e2af1d185ad701529b8e5e477ee46c9e8
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-07-17 14:00:58 +0300

    task-8

--- js/task-07.js
+++ js/task-07.js
@@ -5,5 +5,5 @@ const textEl = document.querySelector('#text');
 fontSizeControlBarEl.addEventListener('input', onSizeControlBarChange);
 
 function onSizeControlBarChange(event) {
-   textEl.style.fontSize = event.target.value + 'px';
+   textEl.style.fontSize = `${event.target.value}px`;
 }
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formEmail = event.currentTarget.elements.email;
+    const formPass = event.currentTarget.elements.password;
+
+    if (formEmail.value === "" || formPass.value === "") {
+        return alert('Всі поля повинні бути заповнені!')
+    }
+
+    const formUserData = {
+        Email: formEmail.value,
+        Password: formPass.value,
+    }
+    console.log(formUserData);
+
+    event.currentTarget.reset();
+}
