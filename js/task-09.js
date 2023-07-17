332f1404503ee0191d9cdaf1f840b8a4e5ea784f
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-07-17 19:42:32 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bcgColorSpanEl = document.querySelector('.color')
+const changeColorBtnEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+
+changeColorBtnEl.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  const bcgColor = getRandomHexColor();
+  
+  bcgColorSpanEl.textContent = bcgColor;
+  bodyEl.style.backgroundColor = bcgColor;
+}
