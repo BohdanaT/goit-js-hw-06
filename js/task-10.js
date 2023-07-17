b86ff922aee2acbca1a4c649c25de8c99845f335
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-07-17 21:33:58 +0300

    task-10(unfinished)

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  controls: document.querySelector('#controls'),
+  boxes: document.querySelector('#boxes'),
+  createBtn: document.querySelector('button[data-create]'),
+  destroyBtn: document.querySelector('button[data-destroy]'),
+}
+
+refs.createBtn.addEventListener('click', createBoxes);
+
+function createBoxes(amount) {
+  const divElement = document.createElement('div');
+  divElement.style.width = '30px';
+  divElement.style.height = '30px';
+  divElement.style.backgroundColor = getRandomHexColor();
+  
+  refs.boxes.appendChild(divElement);
+}
+
+const controlsValue = Number(refs.controls.firstElementChild.value)
+createBoxes(controlsValue);
